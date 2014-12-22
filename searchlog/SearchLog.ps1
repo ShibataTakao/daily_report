d9431448738e4f5874f5d66c4d8c457ba40f2e5b
柴田崇夫 <shibata.takao@isid.co.jp>
2014-12-22 09:05:47 +0900

    add searchlog

---
+++ searchlog/SearchLog.ps1
@@ -0,0 +1,23 @@
+﻿$inDir = "c:\shibata\note\2014"
+$outFile = "c:\shibata\tmp\search.md"
+
+$output = @()
+
+Get-ChildItem $inDir | sort -descending | foreach{
+  $output += "# $_`n"
+  $inTargetSection = $FALSE
+  Get-Content $_.Fullname -Encoding UTF8 | foreach{
+    $str = $_
+
+    if($str -eq "# 業務記録"){
+      $inTargetSection = $TRUE
+    }elseif($str -eq "----"){
+      $inTargetSection = $FALSE
+    }elseif($inTargetSection){
+      $output += $str
+    }
+  }
+  $output += "`n----`n"
+}
+
+([string]::Join("`n", $output)) | Out-File $outFile -Encoding UTF8
---
+++ searchlog/searchlog.bat
@@ -0,0 +1,7 @@
+@echo off
+set BIN="C:\Program Files\Sublime Text 3\subl.exe"
+set FILE=C:\shibata\tmp\search.md
+echo Œ‹‡ŠJn
+powershell .\SearchLog.ps1
+echo Œ‹‡I—¹
+%BIN% %FILE%
