c420af4d86261ff481c04e743197dd12968e0965
柴田崇夫 <shibata.takao@isid.co.jp>
2016-08-29 09:33:55 +0900

    note.mdは余り参照していないため、makelogした際に開かないよう修正した

--- makelog/makelog.bat
+++ makelog/makelog.bat
@@ -2,12 +2,10 @@
 
 set BIN="C:\Program Files\Sublime Text 3\subl.exe"
 set FILE_DAILY=D:\shibata\note\current\%date:~0,4%%date:~5,2%%date:~8,2%.md
-set FILE_NOTE=D:\shibata\note\note.md
 set TEMPLATE=D:\shibata\src\script\logging\makelog\template.md
 
 IF NOT EXIST %FILE_DAILY% (
 	copy %TEMPLATE% %FILE_DAILY%
 )
 
-%BIN% %FILE_NOTE%
 %BIN% %FILE_DAILY%
