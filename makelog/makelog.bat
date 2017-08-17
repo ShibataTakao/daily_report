b2677b69dcdf9282e91145179c75beff510a15b2
ShibataTakao <shibata.takao@isid.co.jp>
2017-08-17 18:13:57 +0900

    note.mdを開くのを止めた

--- makelog/makelog.bat
+++ makelog/makelog.bat
@@ -2,12 +2,10 @@
 
 set BIN="C:\Program Files (x86)\Microsoft VS Code\Code.exe"
 set FILE_DAILY=D:\shibata\note\current\%date:~0,4%%date:~5,2%%date:~8,2%.md
-set FILE_NOTE=D:\shibata\note\note.md
 set TEMPLATE=D:\shibata\src\script\logging\makelog\template.md
 
 IF NOT EXIST %FILE_DAILY% (
 	copy %TEMPLATE% %FILE_DAILY%
 )
 
-%BIN% %FILE_NOTE%
 %BIN% %FILE_DAILY%
