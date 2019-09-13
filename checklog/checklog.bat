89bda554d5910f21e44847b04b1f99dcd244d75c
ShibataTakao <shibata.takao@isid.co.jp>
2019-09-13 18:57:38 +0900

    日報の保存先ディレクトリを変更

--- checklog/checklog.bat
+++ checklog/checklog.bat
@@ -1,4 +1,4 @@
 @echo off
-set INFILE=D:\shibata\note\%date:~0,4%%date:~5,2%%date:~8,2%.md
+set INFILE=D:\shibata\dailyreport\%date:~0,4%%date:~5,2%%date:~8,2%.md
 powershell .\CheckLog.ps1 -inFile %INFILE%
 pause
--- makelog/makelog.bat
+++ makelog/makelog.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set BIN="code"
-set FILE_DAILY=D:\shibata\note\%date:~0,4%%date:~5,2%%date:~8,2%.md
+set FILE_DAILY=D:\shibata\dailyreport\%date:~0,4%%date:~5,2%%date:~8,2%.md
 set TEMPLATE=D:\shibata\src\daily_report\makelog\template.md
 
 IF NOT EXIST %FILE_DAILY% (
